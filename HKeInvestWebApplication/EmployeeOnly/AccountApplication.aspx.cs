75c6be8f49b9dd2564251b9d0b7cb07b0ea10424
Robert Olsthoorn <rolsthoorn12@gmail.com>
2016-04-17 20:54:33 -0700

    Fixing broken files

--- HKeInvestWebApplication/EmployeeOnly/AccountApplication.aspx.cs
+++ HKeInvestWebApplication/EmployeeOnly/AccountApplication.aspx.cs
@@ -29,32 +29,44 @@ namespace HKeInvestWebApplication
             return year + "-" + month + "-" + day;
         }
 
-        //BROKEN CODE since SELECT statement returns one row null. Will fix monday
         private string GenerateNextKey(string lastname)
         {
             string lastletters = "";
             if (lastname.Length == 1)
             {
-                lastletters = lastname.Substring(0,1)+lastname.Substring(0,1);
+                lastletters = lastname.Substring(0, 1) + lastname.Substring(0, 1);
             }
             else
             {
                 lastletters = lastname.Substring(0, 2);
             }
             lastletters = lastletters.ToUpper();
-            string sql = "SELECT MAX(accountNumber) FROM Account WHERE AccountNumber LIKE " + "'"+lastletters+"%'";
+            string sql = "SELECT MAX(accountNumber) as max FROM Account WHERE AccountNumber LIKE " + "'" + lastletters + "%'";
             DataTable dtClient = myHKeInvestData.getData(sql);
             string accountNumber = "";
-            if(dtClient.Rows.Count == 0)
+            if (dtClient == null || dtClient.Rows.Count == 0)
             {
                 accountNumber = lastletters + "00000000";
             }
             else
             {
-                string prevAccountNumber = dtClient.Rows[0].ToString();
-                int nextNumber = Int32.Parse(prevAccountNumber);
-                nextNumber++;
-                accountNumber = lastletters + nextNumber;
+                string prevAccountNumber = dtClient.Rows[0]["max"].ToString();
+                if (prevAccountNumber.Length != 10)
+                {
+                    accountNumber = lastletters + "00000000";
+                }
+                else
+                {
+                    int nextNumber = Int32.Parse(prevAccountNumber.Substring(2, 8));
+                    nextNumber++;
+                    string tempNumber = nextNumber.ToString();
+                    accountNumber = lastletters;
+                    for (int i = 8; i > tempNumber.Length; i--)
+                    {
+                        accountNumber += "0";
+                    }
+                    accountNumber += tempNumber;
+                }
             }
             return accountNumber;
         }
@@ -63,93 +75,103 @@ namespace HKeInvestWebApplication
         {
             if (Page.IsValid)
             {
+                try
+                {
+                    string accountNumber = GenerateNextKey(LastName.Text.Trim());
+                    decimal balance1 = Convert.ToDecimal(ChequeAmount.Text);
+                    decimal balance2 = Convert.ToDecimal(TransferForm.Text);
+                    decimal balance = balance1 + balance2;
 
-                string accountNumber = GenerateNextKey(LastName.Text.Trim());
-                var balance1 = Convert.ToDecimal(ChequeAmount.Text);
-                var balance2 = Convert.ToDecimal(TransferForm.Text);
-                balance1 = balance1 + balance2;
+                    string updateAccount = "INSERT INTO Account VALUES (" + "'" + accountNumber + "','" +
+                    RadioButtonList1.SelectedValue + "','" +
+                    balance + "','" +
+                    PrimarySource.SelectedValue + "','" +
+                    OtherInformation.Text + "','" +
+                    InvestmentObjective.SelectedValue + "','" +
+                    InvestmentKnowledge.SelectedValue + "','" +
+                    AnnualIncome.SelectedValue + "','" +
+                    LiquidWorth.SelectedValue + "','" +
+                    FreeCreditSwee.SelectedValue + "','" +
+                    null + "')";
 
-                string updateAccount = "INSERT INTO Account VALUES (" + "'" + accountNumber + "','" +
-                RadioButtonList1.SelectedValue + "','" +
-                balance1 +"','"+
-                PrimarySource.SelectedValue + "','" +
-                OtherInformation.Text+"','"+
-                InvestmentObjective.SelectedValue + "','" +
-                InvestmentKnowledge.SelectedValue + "','" +
-                AnnualIncome.SelectedValue + "','" +
-                LiquidWorth.SelectedValue + "','" +
-                FreeCreditSwee.SelectedValue + "')";
+                    SqlTransaction trans1 = myHKeInvestData.beginTransaction();
+                    myHKeInvestData.setData(updateAccount, trans1);
+                    myHKeInvestData.commitTransaction(trans1);
 
-                SqlTransaction trans1 = myHKeInvestData.beginTransaction();
-                myHKeInvestData.setData(updateAccount, trans1);
-                myHKeInvestData.commitTransaction(trans1);
+                    string updateClient = "INSERT INTO Client VALUES ('" + accountNumber +
+                        "','" + cbTitle.SelectedValue +
+                        "','" + FirstName.Text + "'" + "," +
+                        "'" + LastName.Text + "'" + "," +
+                        "'" + FormatDateToSQL(DateOfBirth.Text) + "'" + "," +
+                        "'" + Email.Text + "'" + "," +
+                        "'" + Building.Text + "'" + "," +
+                        "'" + Street.Text + "'" + "," +
+                       "'" + District.Text + "'" + "," +
+                       "'" + HomePhone.Text + "'" + "," +
+                        "'" + HomeFax.Text + "'" + "," +
+                        "'" + BusinessPhone.Text + "'" + "," +
+                        "'" + MobilePhone.Text + "'" + "," +
+                        "'" + CitizenshipCountry.Text + "'" + "," +
+                        "'" + ResidenceCountry.Text + "'" + "," +
+                        "'" + HKID.Text + "'" + "," +
+                        "'" + PassportCountry.Text + "'" + "," +
+                         "'" + cbEmploymentStatus.Text + "'" + "," +
+                        "'" + SpecificOccupation.Text + "'" + "," +
+                        "'" + EmployYears.Text + "'" + "," +
+                        "'" + EmployName.Text + "'" + "," +
+                        "'" + EmployPhone.Text + "'" + "," +
+                        "'" + BusinessNature.Text + "'" + "," +
+                         "'" + IsEmployedFinancial.SelectedValue + "'" + "," +
+                        "'" + IsInIPO.SelectedValue + "'" + "," +
+                         "'" + FormatDateToSQL(SignedOn.Text) + "'" + ")";
 
-                string updateClient = "INSERT INTO Client VALUES ('" +accountNumber +
-                    "'" + FirstName.Text + "'" + "," +
-                    "'" + LastName.Text + "'" + "," +
-                    "'" + FormatDateToSQL(DateOfBirth.Text) + "'" + "," +
-                    "'" + Email.Text + "'" + "," +
-                    "'" + Building.Text + "'" + "," +
-                    "'" + Street.Text + "'" + "," +
-                   "'" + District.Text + "'" + "," +
-                   "'" + HomePhone.Text + "'" + "," +
-                    "'" + HomeFax.Text + "'" + "," +
-                    "'" + BusinessPhone.Text + "'" + "," +
-                    "'" + MobilePhone.Text + "'" + "," +
-                    "'" + CitizenshipCountry.Text + "'" + "," +
-                    "'" + ResidenceCountry.Text + "'" + "," +
-                    "'" + HKID.Text + "'" + "," +
-                    "'" + PassportCountry.Text + "'" + "," +
-                     "'" + cbEmploymentStatus.Text + "'" + "," +
-                    "'" + SpecificOccupation.Text + "'" + "," +
-                    "'" + EmployYears.Text + "'" + "," +
-                    "'" + EmployName.Text + "'" + "," +
-                    "'" + EmployPhone.Text + "'" + "," +
-                    "'" + BusinessNature.Text + "'" + "," +
-                     "'" + IsEmployedFinancial.SelectedValue + "'" + "," +
-                    "'" + IsInIPO.SelectedValue + "'" + "," +
-                     "'" + FormatDateToSQL(SignedOn.Text) + "'" + ")";
-
-                SqlTransaction trans2 = myHKeInvestData.beginTransaction();
-                myHKeInvestData.setData(updateClient, trans2);
-                myHKeInvestData.commitTransaction(trans2);
+                    SqlTransaction trans2 = myHKeInvestData.beginTransaction();
+                    myHKeInvestData.setData(updateClient, trans2);
+                    myHKeInvestData.commitTransaction(trans2);
 
 
-                if(!CODateOfBirth.Text.Equals("") && !COSignedOn.Text.Equals("")){
+                    if (!RadioButtonList1.SelectedValue.Equals("individual"))
+                    {
 
-                    string updateCOClient = "INSERT INTO Client Values (" +
-                     "'" + COFirstName.Text + "'" + "," +
-                    "'" + COLastName.Text + "'" + "," +
-                    "'" + FormatDateToSQL(CODateOfBirth.Text) + "'" + "," +
-                    "'" + COEmail.Text + "'" + "," +
-                    "'" + COBuilding.Text + "'" + "," +
-                    "'" + COStreet.Text + "'" + "," +
-                    "'" + CODistrict.Text + "'" + "," +
-                    "'" + COHomePhone.Text + "'" + "," +
-                    "'" + COHomeFax.Text + "'" + "," +
-                    "'" + COBusinessPhone.Text + "'" + "," +
-                    "'" + COMobilePhone.Text + "'" + "," +
-                    "'" + COCitizenshipCountry.Text + "'" + "," +
-                    "'" + COResidenceCountry.Text + "'" + "," +
-                    "'" + COHKID.Text + "'" + "," +
-                    "'" + COPassportCountry.Text + "'" + "," +
+                        string updateCOClient = "INSERT INTO Client Values ('" + accountNumber + "'," +
+                        "'" + COcbTitle.SelectedValue + "'," +
+                            "'" + COFirstName.Text + "'" + "," +
+                        "'" + COLastName.Text + "'" + "," +
+                        "'" + FormatDateToSQL(CODateOfBirth.Text) + "'" + "," +
+                        "'" + COEmail.Text + "'" + "," +
+                        "'" + COBuilding.Text + "'" + "," +
+                        "'" + COStreet.Text + "'" + "," +
+                        "'" + CODistrict.Text + "'" + "," +
+                        "'" + COHomePhone.Text + "'" + "," +
+                        "'" + COHomeFax.Text + "'" + "," +
+                        "'" + COBusinessPhone.Text + "'" + "," +
+                        "'" + COMobilePhone.Text + "'" + "," +
+                        "'" + COCitizenshipCountry.Text + "'" + "," +
+                        "'" + COResidenceCountry.Text + "'" + "," +
+                        "'" + COHKID.Text + "'" + "," +
+                        "'" + COPassportCountry.Text + "'" + "," +
+                        "'" + COcbEmploymentStatus.Text + "'" + "," +
+                        "'" + COSpecificOccupation.Text + "'" + "," +
+                        "'" + COEmployYears.Text + "'" + "," +
+                        "'" + COEmployName.Text + "'" + "," +
+                        "'" + COEmployPhone.Text + "'" + "," +
+                        "'" + COBusinessNature.Text + "'" + "," +
+                        "'" + COIsEmployedFinancial.SelectedValue + "'" + "," +
+                        "'" + COIsInIPO.SelectedValue + "'" + "," +
+                        "'" + FormatDateToSQL(COSignedOn.Text) + "'" +
+                        ")";
 
-                    "'" + COcbEmploymentStatus.Text + "'" + "," +
-                    "'" + COSpecificOccupation.Text + "'" + "," +
-                    "'" + COEmployYears.Text + "'" + "," +
-                    "'" + COEmployName.Text + "'" + "," +
-                    "'" + COEmployPhone.Text + "'" + "," +
-                    "'" + COBusinessNature.Text + "'" + "," +
+                        SqlTransaction trans3 = myHKeInvestData.beginTransaction();
+                        myHKeInvestData.setData(updateCOClient, trans3);
+                        myHKeInvestData.commitTransaction(trans3);
 
-                    "'" + COIsEmployedFinancial.SelectedValue + "'" + "," +
-                    "'" + COIsInIPO.SelectedValue + "'" + "," +
-
-                    "'" + FormatDateToSQL(COSignedOn.Text) + "'" +
-                    ")";
-
-                    SqlTransaction trans3 = myHKeInvestData.beginTransaction();
-                    myHKeInvestData.setData(updateCOClient, trans3);
-                    myHKeInvestData.commitTransaction(trans3);
+                        Console.WriteLine("Updated Scuessfully");
+                        IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+                    }
+                }
+                catch (Exception f)
+                {
+                    Console.WriteLine("error thrown: " + f);
                 }
             }
             else
